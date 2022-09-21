03e876d012ff664940219d7f279fbc0eecf6f76a
Robert <robert.cram@gmail.com>
2022-09-21 11:10:37 +0000

    Solves day 9.

--- day09/day09.jl
+++ day09/day09.jl
@@ -6,42 +6,36 @@ include("./../aoc.jl")
 
 using .AOC
 
+@enum FormatVersion V1 V2
+
 function AOC.processinput(data)
     data = split(data, '\n')
 end
 
-function decompressstep(compressed)
+function decompressedlength(compressed, formatversion)
     lb = findfirst('(', compressed)
     rb = findfirst(')', compressed)
-    lb === nothing && return compressed, true
+    lb === nothing && return length(compressed)
 
-    length, repeats = parse.(Int, split(compressed[lb+1:rb-1], 'x'))
-    part2repeat = replace(compressed[rb+1:rb+length], "(" => "[", ")" => "]")
-    compressed[begin:lb-1] * repeat(part2repeat, repeats) * compressed[rb+length+1:end], false
-end
-
-function decompress(compressed)
-    decompressed = compressed
-    while true
-        decompressed, finished = decompressstep(decompressed)
-        finished && break
-    end
-    decompressed
+    len, repeats = parse.(Int, split(compressed[lb+1:rb-1], 'x'))
+    part2repeat = compressed[rb+1:rb+len]
+    formatversion == V1 && (part2repeat = replace(part2repeat, "(" => "[", ")" => "]"))
+    return (lb-1) + repeats * decompressedlength(part2repeat, formatversion) + decompressedlength(compressed[rb+len+1:end], formatversion)
 end
 
 function solvepart1(lines)
-    map(line-> length(decompress(line)), lines)
+    map(line-> decompressedlength(line, V1), lines)
 end
 
 function solvepart2(lines)
-    map(line-> length(decompress(line)), lines)
+    map(line-> decompressedlength(line, V2), lines)
 end
 
 puzzles = [
     Puzzle(9, "test 1", "input-test1.txt", solvepart1, [6, 7, 9, 11, 6, 18]),
     Puzzle(9, "deel 1", solvepart1, [107035]),
-    # Puzzle(9, "test 2", "input-test1.txt", solvepart2, nothing),
-    # Puzzle(9, "deel 2", solvepart2, nothing)
+    Puzzle(9, "test 2", "input-test2.txt", solvepart2, [9, 20, 241920, 445]),
+    Puzzle(9, "deel 2", solvepart2, [11451628995])
 ]
 
 printresults(puzzles)
